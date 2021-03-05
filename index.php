a7280f4fbbd4c97ad7a334efba5c1ecbe6df9490
Eminerve <60135711+Eminerve@users.noreply.github.com>
2021-03-05 09:07:36 +0100

    modification titre et prix

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=0.9">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Väggen.se</title>
+  </head>
+  <body>
+
+
+    <?php include 'navigation.php'; ?>
+
+
+
+    <div class="en_cour">
+      <div class="concept">
+        <h2>Concept</h2>
+        <p>Väggen offers to artists the way to have a free expression wall and more, an exhibition wall around Stockholm valuing by that, the district, the graffiti, and the Street Art.</p>
+        <p>Väggen will be divided in three sections to diversify the artistic approaches:
+          <ul>
+            <li>•	<strong>The daily</strong> wall where you can test, do quick pieces, and be initiated to graffiti.</li><br>
+            <li>•	<strong>The weekly</strong> wall which will used by artists who want to take their time to work on their composition.</li><br>
+            <li>•	<strong>The monthly</strong> wall is an open-air gallery which invites one artist per month and offers to him to be discovered during this period.</li>
+          </ul>
+        </p>
+        <p>Curated by <strong>Nabil Dorbane</strong> et <a href="https://konstjord.com/" target="_blank"><strong>KonstJörd</strong></a> </p>
+      </div>
+
+      <div class="opening">
+        <h2>Opening</h2>
+        <p>The Väggen will invite to the opening the Artist who will paint for us on <strong>March 13</strong> and let his art on the wall until <strong>April 9</strong>. <br>We hope that many of you will come to meet this artist and his art. <br>C'est au pied du mur qu'on voit le mieux le mur...</p>
+
+          <div class="artist_work">
+            <img src="images/image_1.jpg" width="400px" height="auto" alt="artist1"><img src="images/image_2.jpg" width="400px" height="auto" alt="artist1">
+          </div>
+      </div>
+
+      <div class="contact">
+        <h2>contact</h2>
+        <p>The walls are bookable ... please contact Nabil Dorbane for availability. <br>You can reach him on 07 898 34 454.</p>
+      </div>
+    </div>
+
+
+  </body>
+</html>
---
+++ navigation.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>navigation</title>
+  </head>
+  <header>
+    <div class="logo">
+      <a href="index.php"><img src="images/test-logo1.png" alt="logo väggen vaggen" width="100px" height="100px"></a>
+    </div>
+
+    <div class="barre_nav">
+      <ul>
+        <li> <a href="#">Video</a></li>
+        <li><a href="#">Daily</a></li>
+        <li><a href="#">Weekly</a></li>
+        <li><a href="#">Monthly</a></li>
+      </ul>
+    </div>
+  </header>
+
+
+<body>
+  </body>
+</html>
